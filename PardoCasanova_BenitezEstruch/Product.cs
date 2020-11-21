9623293cf58c4d6c6ede42f626fd6cd22bcff38e
Ruben <rubenpardocasanova@gmail.com>
2020-11-21 13:31:26 +0100

    Utility y Clases

--- PardoCasanova_BenitezEstruch/Product.cs
+++ PardoCasanova_BenitezEstruch/Product.cs
@@ -2,18 +2,195 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PardoCasanova_BenitezEstruch
 {
     class Product
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string type { get; set; }
-        public string manufacturer { get; set; }
-        public string description { get; set; }
-        public int price { get; set; }
-        public int stock { get; set; }
+        // atributes
+        private string id;// auto generated unique value
+        private string name;
+        private string type;
+        private string manufacturer;
+        private string description;
+        private string price;
+        private string stock;
+
+        private string[] typesAllowed = { "sensor", "micro" };
+
+        // properties
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                if (!Utility.isTextEmpty(id))
+                {
+                    id = value;
+                }
+                else
+                {
+                    throw new ArgumentException("ID can not be empty");
+                }
+            }
+        }
+
+        public string NAME
+        {
+            get { return name; }
+            // it can't be empty text and will be always normalize "text->"Text"
+            set
+            {
+                if (!Utility.isTextEmpty(value))
+                {
+                    name = Utility.normalizeText(value);
+                }
+                else
+                {
+                    throw new ArgumentException("Name can not be empty");
+                }
+
+            }
+        }
+
+        public string TYPE
+        {
+            get { return type; }
+            // only can take the "SENSOR"|"MICRO" values, will be normalize
+            set
+            {
+                if (isValidType(value))
+                {
+                    type = value.ToUpper();
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid type of product");
+
+                }
+
+            }
+        }
+
+        public string MANUFACTURER
+        {
+            get { return manufacturer; }
+            // it can't be empty text and will be always normalize "text->"Text"
+            set
+            {
+                if (!Utility.isTextEmpty(value))
+                {
+                    manufacturer = Utility.normalizeText(value);
+                }
+                else
+                {
+                    throw new ArgumentException("Manufacturer can not be empty");
+                }
+
+            }
+        }
+
+        public string DESCRIPTION
+        {
+            get { return description; }
+            // it can be empty text and will be always normalize "text->"Text"
+            set
+            {
+                description = Utility.normalizeText(value);
+            }
+        }
+
+        public string PRICE
+        {
+            get { return price; }
+            // try to convert to double and not negative
+            set
+            {
+                if (isPositiveDecimalValue(value))
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Price should be a positive number");
+                }
+            }
+        }
+        public string STOCK
+        {
+            get { return stock; }
+            set
+            {
+                // only positive integer numbers
+                if (isPositiveIntegerValue(value))
+                {
+                    stock = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Stock should be a positive integer number");
+                }
+            }
+        }
+
+
+        // constructor
+        public Product(string id, string name, string type, string manufacturer, string description, string price, string stock)
+        {
+            ID = id;
+            NAME = name;
+            TYPE = type;
+            MANUFACTURER = manufacturer;
+            DESCRIPTION = description;
+            PRICE = price;
+            STOCK = stock;
+        }
+
+        // sirve para cuando creamos un producto nuevo 
+        public Product()
+        {
+            ID = Utility.generateID();
+
+        }
+
+        // methods 
+        private bool isValidType(string type)
+        {
+            return typesAllowed.Contains(type.ToLower());
+        }
+
+        //regex \b^[0-9]\d*\b no puede ser porque permite poner 2323.11212 y lo detecta como dos
+        // por tanto se debe usar esto: ^[0-9]\d*$ el $ indica que es solo una unica cadena, sin puntos ni espacios permitidos
+        // ponemos el ^ delante para solo pillar numeros, de otra forma si se pone un espacio lo pilla o un punto
+        private bool isPositiveIntegerValue(string number)
+        {
+
+
+            Match match = Regex.Match(number, @"^[0-9]\d*$");
+
+            return match.Success;
+
+        }
+        // regex = ^[0-9]\d*.\d*$ -> lo mismo que antes pero el valor es solo si son digitos positvios separados por un punto
+        private bool isPositiveDecimalValue(string number)
+        {
+
+            Match matchInteger = Regex.Match(number, @"^[0-9]\d*$");
+            // poner 12 de precio tambien es valido
+            if (matchInteger.Success)
+            {
+                return true;
+            }
+            Match matchDecimal = Regex.Match(number, @"^[0 - 9]\d *.\d * $");
+            if (matchDecimal.Success)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
     }
 }
---
+++ PardoCasanova_BenitezEstruch/Utility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PardoCasanova_BenitezEstruch
+{
+    class Utility
+    {
+        // text -> Text
+        public static string normalizeText(String textToNormalize)
+        {
+            textToNormalize = textToNormalize.ToLower();
+            if (textToNormalize.Length == 0)
+                return "";
+            else if (textToNormalize.Length == 1)
+                return char.ToUpper(textToNormalize[0]) + "";
+            else
+                return (char.ToUpper(textToNormalize[0]) + textToNormalize.Substring(1));
+
+        }
+
+        /**
+         * a GUID will never be generated twice by any computer or group of computers in existence. 
+         * The total number of unique keys (2^128 or 3.4×10^38 - in relation there are about 1.33×10^50 atoms on earth) 
+         * is so large that the probability of the same number being generated twice is extremely small, 
+         * and certain techniques have been developed to help ensure that numbers are not duplicated
+         */
+        public static string generateID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static bool isTextEmpty(string text)
+        {
+            text = text.Trim();
+            if (text.Equals(""))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void showDialogError(String title, String message)
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+    }
+}
